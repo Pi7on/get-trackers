acb9a895e42f52d1b27c42c6cefbc3742cd137f4
Pi7on <45372614+Pi7on@users.noreply.github.com>
2021-08-25 16:32:31 +0200

    add progress bar

--- get_trackers.bat
+++ get_trackers.bat
@@ -1,4 +1,3 @@
 @echo off
-curl "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt"|clip
+curl --progress-bar "https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"|clip
 echo Latest trackers copied to clipboard!
-@pause
